4c1b3487d64a7a9eeecf83921f7a766c87e48060
Nick Bennett <nick@nickbenn.com>
2020-06-22 15:54:30 -0600

    UPdates with deletions.

--- src/main/java/edu/cnm/deepdive/quotes/controller/SourceController.java
+++ src/main/java/edu/cnm/deepdive/quotes/controller/SourceController.java
@@ -3,9 +3,13 @@ package edu.cnm.deepdive.quotes.controller;
 import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.service.SourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +27,12 @@ public class SourceController {
   public Iterable<Source> get() {
     return sourceRepository.getAllByOrderByNameAsc();
   }
-  
+
+  @PostMapping(
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.CREATED)
+  public Source post(@RequestBody Source source) {
+    return sourceRepository.save(source);
+  }
+
 }
