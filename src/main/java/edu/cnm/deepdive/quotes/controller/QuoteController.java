ffad1519b7342759833bed0d679ca5fd2d2845f4
Nick Bennett <nick@nickbenn.com>
2020-07-09 20:05:25 -0600

    Quote source PUT.

--- src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.quotes.controller;
 
 import edu.cnm.deepdive.quotes.model.entity.Quote;
+import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.model.entity.Tag;
 import edu.cnm.deepdive.quotes.service.QuoteRepository;
 import edu.cnm.deepdive.quotes.service.SourceRepository;
@@ -74,6 +75,17 @@ public class QuoteController {
   @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Quote> search(@RequestParam(name = "q", required = true) String filter) {
     return quoteRepository.getAllByTextContainingOrderByTextAsc(filter);
+  }
+
+  @PutMapping(value = "/{id:\\d+}/source",
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Quote putSource(@PathVariable long id, @RequestBody Source source) {
+    Quote quote = get(id);
+    if (source != null && source.getId() != null) {
+      source = sourceRepository.findById(source.getId()).orElseThrow(NoSuchElementException::new);
+    }
+    quote.setSource(source);
+    return quoteRepository.save(quote);
   }
 
 }
